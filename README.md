a936c5c0eebe42c86f245e3015e45c17b48e0fbb
Yiğit Kaan Korkmaz <77935633+Evansmuscle@users.noreply.github.com>
2022-05-20 13:26:10 +0300

    feat: finish app

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Create React components with boilerplate without the hassle.
 Just run the command line tool with the command **rcc "Component Name Here"** and you're set!
 
 To disable TypeScript, run with the argument **--no-ts**
+
+The default test suite is Cypress, you can change it to jest by using the **--use-jest** flag.
--- src/main.go
+++ src/main.go
@@ -26,23 +26,108 @@ func makeFile(fileName string, content string) {
 
 func main() {
 	args := make(map[int]string, len(os.Args[1:]))
+	
+	noTs := false
+	useJest := false
 
 	for i := 0; i < len(os.Args[1:]); i++ {
 		args[i] = os.Args[1:][i]	
+
+		if args[i] == "--no-ts" {
+			noTs = true
+		}
+		
+		if args[i] == "--use-jest" {
+			useJest = true
+		}
+
+		fmt.Println(args[i])
 	}
 	
 	if componentName, ok := args[0]; ok {
-		tsxFile := "import './" + componentName + "';\n\nimport React from 'react';\n\ninterface " + componentName + "Props { }\n\n const " + componentName + " = () => {\n\treturn <div>Hello World</div>\n}\n\n export default " + componentName
-		exportFile := "import " + componentName + " from './" + componentName + "'\n\n export default " + componentName
+		if noTs {
+			tsxFile := "import './" + componentName + ".scss';\n\nimport React from 'react';\n\nconst " + componentName + " = () => {\n\treturn <div>Hello World</div>\n}\n\n export default " + componentName
+			exportFile := "import " + componentName + " from './" + componentName + "'\n\nexport default " + componentName
+			testFile := ""
+			if useJest {
+				testFile = `import renderer from 'react-test-renderer';
+import ` + componentName + ` from './` + componentName +`';
+
+it('Renders components', () => {
+    const component = renderer.create(
+   	<` + componentName + ` />
+    )
+    
+    let tree = component.toJSON();
+    
+    // Your test code here..
+
+    expect(tree).toMatchSnapshot();
+});`
+			} else {
+				testFile = `import React from 'react';
+import { mount } from '@cypress/react';
+import ` + componentName + ` from './` + componentName +`';
+
+it('Renders components', () => {
+   mount(<` + componentName + ` />);
+
+   // Your test code here..
+});`
+			}
+
+			makeFolder(componentName)
+
+			err := os.Chdir("./" + componentName)
+			checkErr(err)
+			
+			makeFile(componentName + ".scss", "")
+			makeFile("index.js", exportFile)
+			makeFile(componentName + ".jsx", tsxFile)
+			makeFile(componentName + ".spec.jsx", testFile)
+			
+			return
+		}
+
+		tsxFile := "import './" + componentName + ".scss';\n\nimport React from 'react';\n\ninterface " + componentName + "Props { }\n\nconst " + componentName + " = () => {\n\treturn <div>Hello World</div>\n}\n\n export default " + componentName
+		exportFile := "import " + componentName + " from './" + componentName + "'\n\nexport default " + componentName
+		testFile := ""
+		if useJest {
+			testFile = `import renderer from 'react-test-renderer';
+import ` + componentName + ` from './` + componentName +`';
+
+it('Renders components', () => {
+    const component = renderer.create(
+   	<` + componentName + ` />
+    )
+    
+    let tree = component.toJSON();
+    
+    // Your test code here..
+
+    expect(tree).toMatchSnapshot();
+});`
+		} else {
+			testFile = `import React from 'react';
+import { mount } from '@cypress/react';
+import ` + componentName + ` from './` + componentName +`';
+
+it('Renders components', () => {
+   mount(<` + componentName + ` />);
+
+   // Your test code here..
+});`
+		}
 
 		makeFolder(componentName)
 
 		err := os.Chdir("./" + componentName)
 		checkErr(err)
 		
-		makeFile(componentName + ".tsx", tsxFile)
 		makeFile(componentName + ".scss", "")
 		makeFile("index.ts", exportFile)
+		makeFile(componentName + ".tsx", tsxFile)
+		makeFile(componentName + ".spec.tsx", testFile)
 	} else {
 		fmt.Printf("You forgot to input a component name!")
 	}
